d05494111c79be71119c98883cff27ae749869b6
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-07-19 12:15:54 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,9 @@
+'''
+Name:Lokesh
+Date & Day : 18th july 2022 (Monday)
+File Desc: Types of comments in PyThon
+           1) Single line comment
+           2) Multi line comment
+'''
+# Its a single line cooment
+print('Hey today we have seen Comments in Python')
